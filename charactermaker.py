2b1e2c382a8d3893fa57a55d8acbfa004fdf03ee
Adam R <121298839+abtzpro@users.noreply.github.com>
2023-04-22 11:42:27 -0500

    Update charactermaker.py
    Add- Ability to save user data and character creation data for later load by file

--- charactermaker.py
+++ charactermaker.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 class Character:
     def __init__(self):
@@ -26,6 +27,19 @@ class Character:
         self.wisdom = stats[4]
         self.charisma = stats[5]
         
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "race": self.race,
+            "job": self.job,
+            "strength": self.strength,
+            "dexterity": self.dexterity,
+            "constitution": self.constitution,
+            "intelligence": self.intelligence,
+            "wisdom": self.wisdom,
+            "charisma": self.charisma
+        }
+        
     def __str__(self):
         return f"Name: {self.name}\nRace: {self.race}\nJob: {self.job}\nStrength: {self.strength}\nDexterity: {self.dexterity}\nConstitution: {self.constitution}\nIntelligence: {self.intelligence}\nWisdom: {self.wisdom}\nCharisma: {self.charisma}"
         
@@ -37,5 +51,44 @@ def create_character():
     character.job = input("Enter a job for your character: ")
     character.assign_stats()
     return character
-    
-print(create_character())
+
+def save_character(character, filename):
+    with open(filename, "w") as f:
+        json.dump(character.to_dict(), f)
+        
+def load_character(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+        character = Character()
+        character.name = data["name"]
+        character.race = data["race"]
+        character.job = data["job"]
+        character.strength = data["strength"]
+        character.dexterity = data["dexterity"]
+        character.constitution = data["constitution"]
+        character.intelligence = data["intelligence"]
+        character.wisdom = data["wisdom"]
+        character.charisma = data["charisma"]
+        return character
+
+def choose_option():
+    choice = input("Enter '1' to create a new character, '2' to save a character, '3' to load a character, or 'q' to quit: ")
+    if choice == "1":
+        return create_character()
+    elif choice == "2":
+        filename = input("Enter a filename to save your character to: ")
+        character = create_character()
+        save_character(character, filename)
+        return character
+    elif choice == "3":
+        filename = input("Enter a filename to load your character from: ")
+        return load_character(filename)
+    elif choice == "q":
+        return None
+    else:
+        print("Invalid choice, please try again.")
+        return choose_option()
+
+if __name__ == "__main__":
+    while True:
+        character = choose_option
